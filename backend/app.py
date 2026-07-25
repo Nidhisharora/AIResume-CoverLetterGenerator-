7e40482c9fd6f42869d2e7651722901a4eae03a1
Nidhisharora <nidhisharora79@gmail.com>
2026-07-25 23:27:05 +0530

    Allow deployed frontend

--- backend/app.py
+++ backend/app.py
@@ -14,7 +14,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173","https://airesume-coverlettergenerator-1.onrender.com"],
+    
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
